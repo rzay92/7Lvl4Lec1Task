d2ccdc77a3472b9b36c8255f7bda13ef23ac0fe0
rzay92 <67005373+rzay92@users.noreply.github.com>
2020-07-29 19:47:26 +0900

    Add files via upload

---
+++ Test/src/kek/Solution.java
@@ -0,0 +1,37 @@
+package kek;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.parseInt;
+
+/*
+Массивный максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] array = initializeArray();
+        int max = max(array);
+        System.out.println(max);
+    }
+
+    public static int[] initializeArray() throws IOException {
+
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
+        }
+        return array;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
